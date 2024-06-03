6a4160ce95f23131c08aecef3e9221d3a7d10539
Huzefa1413 <79566904+Huzefa1413@users.noreply.github.com>
2024-06-03 23:54:33 +0500

    Homepage

--- src/components/Achievements.jsx
+++ src/components/Achievements.jsx
@@ -1,38 +1,71 @@
 import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, A11y, Autoplay } from 'swiper/modules';
 import './styles/Achievements.css';
+import achievement1 from '../assets/achievement-thubmnail-1.png';
+import achievement2 from '../assets/achievement-thubmnail-2.png';
+import achievement3 from '../assets/achievement-thubmnail-3.png';
+import achievement4 from '../assets/achievement-thubmnail-4.png';
+import achievement5 from '../assets/achievement-thubmnail-5.png';
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
 
 const Achievements = () => {
+  const achievementsData = [
+    {
+      imgSrc: achievement1,
+      description:
+        'Our Northern Areas School Network consists of 35 plus registered semi formal schools, providing education in the areas of Thotha, Malsi, Dhanibaklan, Banilanryal, Parhour, Darabatangi, Habibdori (Chnari), Dakhankarbani etc.',
+    },
+    {
+      imgSrc: achievement2,
+      description:
+        'HOPE has 3 formal Schools, located throughout Pakistan, where 1500 + students are studying.',
+    },
+    {
+      imgSrc: achievement3,
+      description:
+        '52 health facilities monitored in earthquake affected areas in AJK.',
+    },
+    {
+      imgSrc: achievement4,
+      description:
+        'HOPE has provided 422,754,120 liters of clean drinking water, in collaboration with P&G “Pak Saaf Pakistan” Project. ',
+    },
+    {
+      imgSrc: achievement5,
+      description:
+        'Around 58,000 patients were seen and 85 delivers were conducted in both MCH center in Manzoor Colony and Mujahid Colony.',
+    },
+  ];
+
   return (
     <div className="achievements container py-5 text-center">
       <h1>What We Have Achieved</h1>
-      <div className="section-wrapper">
-        <div className="section">
-          <div className="item col-lg-4 col-md-6 col-sm-12">
-            <p>Reconstruction of houses in flood affected areas in Sindh.</p>
-            <button>Read More</button>
-          </div>
-          <div className="item col-lg-4 col-md-6 col-sm-12">
-            <p>Reconstruction of houses in flood affected areas in Sindh.</p>
-            <button>Read More</button>
-          </div>
-          <div className="item col-lg-4 col-md-6 col-sm-12">
-            <p>Reconstruction of houses in flood affected areas in Sindh.</p>
-            <button>Read More</button>
-          </div>
-          <div className="item col-lg-4 col-md-6 col-sm-12">
-            <p>Reconstruction of houses in flood affected areas in Sindh.</p>
-            <button>Read More</button>
-          </div>
-          <div className="item col-lg-4 col-md-6 col-sm-12">
-            <p>Reconstruction of houses in flood affected areas in Sindh.</p>
-            <button>Read More</button>
-          </div>
-          <div className="item col-lg-4 col-md-6 col-sm-12">
-            <p>Reconstruction of houses in flood affected areas in Sindh.</p>
-            <button>Read More</button>
-          </div>
-        </div>
-      </div>
+      <Swiper
+        modules={[Navigation, A11y, Autoplay]}
+        slidesPerView={1}
+        navigation
+        autoplay={{ delay: 3000 }}
+        loop={true}
+      >
+        {achievementsData.map((achievement, index) => (
+          <SwiperSlide key={index} className="item">
+            <div
+              className="item-content"
+              style={{ backgroundImage: `url(${achievement.imgSrc})` }}
+            >
+              <div className="text-content">
+                <p>{achievement.description}</p>
+                <button>Read More</button>
+              </div>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </div>
   );
 };
--- src/components/Collaborations.jsx
+++ src/components/Collaborations.jsx
@@ -1,21 +1,69 @@
 import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay } from 'swiper/modules';
 import './styles/Collaborations.css';
+import img1 from '../assets/image1x1.png';
+import img2 from '../assets/image1x2.png';
+import img3 from '../assets/image1x3.png';
+import img4 from '../assets/image1x4.png';
+import img5 from '../assets/image2x1.png';
+import img6 from '../assets/image2x2.png';
+import img7 from '../assets/image2x3.png';
+import img8 from '../assets/image2x4.png';
+import img9 from '../assets/image3x1.png';
+import img10 from '../assets/image3x2.png';
+import img11 from '../assets/image3x3.png';
+import img12 from '../assets/image3x4.png';
+import img13 from '../assets/image4x1.png';
+import img14 from '../assets/image4x2.png';
+import img15 from '../assets/image4x3.png';
+import img16 from '../assets/image4x4.png';
+import img17 from '../assets/image5x1.png';
+import img18 from '../assets/image5x2.png';
+import img19 from '../assets/image5x3.png';
+import img20 from '../assets/image5x4.png';
 
-import img1 from '../assets/row-1-column-1.png';
-import img2 from '../assets/row-2-column-1.png';
-import img3 from '../assets/row-3-column-1.png';
-import img4 from '../assets/row-4-column-1.png';
+const images = [
+  img1,
+  img2,
+  img3,
+  img4,
+  img5,
+  img6,
+  img7,
+  img8,
+  img9,
+  img10,
+  img11,
+  img12,
+  img13,
+  img14,
+  img15,
+  img16,
+  img17,
+  img18,
+  img19,
+  img20,
+];
 
 const Collaborations = () => {
   return (
     <div className="collaborations container py-5 text-center">
       <h1>Major Collaborations</h1>
-      <marquee behavior="" direction="">
-        <img src={img1} alt="" />
-        <img src={img2} alt="" />
-        <img src={img3} alt="" />
-        <img src={img4} alt="" />
-      </marquee>
+      <Swiper
+        modules={[Autoplay]}
+        spaceBetween={10}
+        slidesPerView={'auto'}
+        autoplay={{ delay: 50 }}
+        loop={true}
+        centeredSlides={false}
+      >
+        {images.map((img, index) => (
+          <SwiperSlide key={index}>
+            <img src={img} alt={`Collaboration ${index + 1}`} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </div>
   );
 };
